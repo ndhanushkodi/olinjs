67452da96a2e6a90ea2c2244eb34ac487cd555b7
Nitya Dhanushkodi <nitya.dhanushkodi@students.olin.edu>
2015-02-15 15:47:43 -0500

    class08 in class

--- classes/class08/in_class/tests/server/test.js
+++ classes/class08/in_class/tests/server/test.js
@@ -1,28 +1,31 @@
 
-// // Setup our assertion library
-// var expect = require('chai').expect;
+// Setup our assertion library
+var expect = require('chai').expect;
 
-// var index = require('../../routes/index');
+var index = require('../../routes/index');
 
 
-// // Sample tests
-// describe("A test suite", function() {
-// 	// Syncronous
-// 	it('should use expect syntax', function() { 
-// 		expect(true).to.be.true; 
-// 	});
-
-// 	// Async
-// 	it('should work asyncronously', function(done) {
-// 		setTimeout(function() {
-// 			expect(true).to.be.true;
-// 			done();
-// 		}, 1000);
-// 	});
-// });
+// Sample tests
+describe("A test suite", function() {
+	// Syncronous
+	it('should use expect syntax', function() { 
+		expect(true).to.be.true; 
+	});
+	this.timeout(5000);
+	// Async
+	it('should work asyncronously', function(done) {
+		setTimeout(function() {
+			expect(true).to.be.true;
+			done();
+		}, 1000);
+	});
+});
 
 // describe("index", function() {
 // 	it('should have an attribute ten equal to 10', function() {
 // 		expect(index.ten).to.equal(10);
 // 	});
 // });
+
+//each route has a test suite, each function has an it
+//tell mocha which file to run to test specific route. 
